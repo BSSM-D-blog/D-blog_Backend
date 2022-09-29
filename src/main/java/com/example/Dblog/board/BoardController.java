9024a02fd4385c8240f27bead42f84e3e6db5cac
jeongdonggyeom <topcome928@gmail.com>
2022-09-29 10:21:56 +0900

    FIX :: 파일 저장시 파일명 겹침

--- src/main/java/com/example/Dblog/board/BoardController.java
+++ src/main/java/com/example/Dblog/board/BoardController.java
@@ -2,20 +2,12 @@ package com.example.Dblog.board;
 
 import com.example.Dblog.file.FileDto;
 import com.example.Dblog.file.FileService;
-import com.example.Dblog.user.UserEntity;
-import com.example.Dblog.util.MD5Generator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-
-import javax.validation.Valid;
 import java.io.File;
-import java.net.URI;
-import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @RestController
@@ -29,7 +21,9 @@ public class BoardController {
         try{
             if(files.isPresent()) {
                 String origFilename = files.get().getOriginalFilename();
-                String filename = new MD5Generator(origFilename).toString();
+                String extension = origFilename.substring(origFilename.lastIndexOf("."));
+                String uuid = UUID.randomUUID().toString();
+                assert origFilename != null;
                 String savePath = System.getProperty("user.dir") + "\\files";
                 if(!new File(savePath).exists()){
                     try{
@@ -38,12 +32,12 @@ public class BoardController {
                         e.printStackTrace();
                     }
                 }
-                String filePath = savePath + "\\" + filename;
+                String filePath = savePath + "\\" + uuid+extension;
                 files.get().transferTo(new File(filePath));
 
                 FileDto fileDto = new FileDto();
                 fileDto.setOriginalname(origFilename);
-                fileDto.setFilename(filename);
+                fileDto.setFilename(uuid);
                 fileDto.setFilepath(filePath);
 
                 Long fileId = fileService.saveFile(fileDto);
