73b4ff28b3362a5a701b3a3d2c3ef7eb6ab4de4f
jeongdonggyeom <topcome928@gmail.com>
2022-10-05 17:12:59 +0900

    FEAT :: board update, delete

--- src/main/java/com/example/Dblog/file/FileService.java
+++ src/main/java/com/example/Dblog/file/FileService.java
@@ -8,14 +8,19 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.ServletContext;
 import javax.transaction.Transactional;
 import java.io.File;
-import java.net.URL;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 
 @Slf4j
 @Service
 public class FileService {
     private FileRepository fileRepository;
+    private String savePath = System.getProperty("user.dir") + "\\src\\main\\resources\\images";
 
     @Autowired
     ServletContext servletContext;
@@ -36,26 +41,58 @@ public class FileService {
     }
 
     @Transactional
-    public Long saveFile(MultipartFile file) {
+    public FileEntity saveFile(MultipartFile file) {
         try {
             String originFileName = file.getOriginalFilename();
             assert originFileName != null;
             String extension = originFileName.substring(originFileName.lastIndexOf("."));
             String uuid = UUID.randomUUID().toString();
-            String savePath = System.getProperty("user.dir") + "\\src\\main\\resources\\images";
-            String filePath = savePath + "\\" + uuid + extension;
+            String fileName = uuid + extension;
+            String filePath = savePath + "\\" + fileName;
             file.transferTo(new File(filePath));
-            String saveServerPath = "localhost:8080/images/" + uuid + extension;
+            String saveServerPath = "10.150.149.114:8080/images/" + fileName;
             FileDto fileDto = new FileDto();
             fileDto.setOriginalname(originFileName);
-            fileDto.setFilename(uuid);
-            fileDto.setFilepath(saveServerPath);
+            fileDto.setFilename(fileName);
+            fileDto.setFilepath(filePath);
+            fileDto.setServerPath(saveServerPath);
 
-            return fileRepository.save(fileDto.toEntity()).getId();
+            return fileRepository.save(fileDto.toEntity());
 
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
+    }
+
+    @Transactional
+    public void updateFile(FileEntity entity, Optional<MultipartFile> file) {
+        try {
+            if(file.isPresent()){
+                String originFileName = file.get().getOriginalFilename();
+                String uuid = UUID.randomUUID().toString();
+                assert originFileName != null;
+                String extension = originFileName.substring(originFileName.lastIndexOf("."));
+                String fileName = uuid + extension;
+                String filePath = savePath + "\\" + fileName;
+                file.get().transferTo(new File(filePath));
+                String saveServerPath = "10.150.149.114:8080/images/" + fileName;
+                entity.setOriginalname(originFileName);
+                entity.setFilename(fileName);
+                entity.setFilepath(filePath);
+                entity.setServerPath(saveServerPath);
+                fileRepository.save(entity);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Transactional
+    public boolean deleteFile(FileEntity file){
+        File file1 = new File(savePath + "\\" + file.getFilename());
+        boolean result = file1.delete();
+        fileRepository.delete(file);
+        return result;
     }
 }
