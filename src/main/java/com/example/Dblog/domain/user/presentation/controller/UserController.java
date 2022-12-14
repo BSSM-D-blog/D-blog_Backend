c85489b1325376946800ac45b597a159718e8ef9
jeongdonggyeom <topcome928@gmail.com>
2022-12-14 09:19:47 +0900

    FEAT :: 유저 개인 페이지 api

--- src/main/java/com/example/Dblog/domain/user/presentation/controller/UserController.java
+++ src/main/java/com/example/Dblog/domain/user/presentation/controller/UserController.java
@@ -1,21 +1,21 @@
-package com.example.Dblog.domain.user.controller;
+package com.example.Dblog.domain.user.presentation.controller;
 
 import com.example.Dblog.domain.file.entity.FileEntity;
 import com.example.Dblog.domain.file.service.FileService;
+import com.example.Dblog.domain.user.presentation.dto.UserResponseDto;
 import com.example.Dblog.domain.user.service.UserService;
-import com.example.Dblog.domain.user.dto.UserResponseDto;
 import com.example.Dblog.domain.user.entity.UserEntity;
 import com.example.Dblog.domain.user.entity.repository.UserRepository;
 import com.example.Dblog.global.jwt.JwtTokenProvider;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
 import java.util.Optional;
 
-@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
@@ -25,18 +25,22 @@ public class UserController {
     private final JwtTokenProvider jwtTokenProvider;
     private final FileService fileService;
 
-    @GetMapping("/user")
+    @GetMapping("/userinfo")
     public UserResponseDto findUser(@RequestHeader("Authorization") String token){
-        log.info("accessToken: " + token);
         return userService.findUser(token);
     }
 
-    @PutMapping("/user")
+    @PutMapping("/userinfo")
     public void updateProfile(@RequestParam("file") MultipartFile file, @RequestHeader("Authorization") String token){
         Claims parseToken = jwtTokenProvider.parseJwtToken(token);
         Optional<UserEntity> user = userRepository.findByUsername(parseToken.getSubject());
         FileEntity saveFile = fileService.saveFile(file);
         user.ifPresent(userEntity -> userService.updateProfile(saveFile.getServerPath() , userEntity));
+    }
+
+    @GetMapping("/user/{id}")
+    public Map<String, Object> getPersonalInfo(@PathVariable Long id) {
+        return userService.getPersonalInfo(id);
     }
 }
 
--- src/main/java/com/example/Dblog/domain/user/service/UserService.java
+++ src/main/java/com/example/Dblog/domain/user/service/UserService.java
@@ -1,6 +1,13 @@
 package com.example.Dblog.domain.user.service;
 
-import com.example.Dblog.domain.user.dto.UserResponseDto;
+import com.example.Dblog.domain.board.entity.BoardEntity;
+import com.example.Dblog.domain.board.entity.repository.BoardRepository;
+import com.example.Dblog.domain.board.presentation.dto.BoardResponseDto;
+import com.example.Dblog.domain.board.service.BoardService;
+import com.example.Dblog.domain.category.dto.CategoryDto;
+import com.example.Dblog.domain.category.entity.CategoryEntity;
+import com.example.Dblog.domain.category.entity.repository.CategoryRepository;
+import com.example.Dblog.domain.user.presentation.dto.UserResponseDto;
 import com.example.Dblog.domain.user.entity.UserEntity;
 import com.example.Dblog.domain.user.entity.repository.UserRepository;
 import com.example.Dblog.global.jwt.JwtTokenProvider;
@@ -16,6 +23,9 @@ import java.util.*;
 public class UserService {
     private final UserRepository userRepository;
     private final JwtTokenProvider jwtTokenProvider;
+    private final CategoryRepository categoryRepository;
+    private final BoardRepository boardRepository;
+    private final BoardService boardService;
 
     @Transactional
     public void updateProfile(String path, UserEntity user){
@@ -28,6 +38,34 @@ public class UserService {
         Claims parseToken = jwtTokenProvider.parseJwtToken(token);
         Optional<UserEntity> user = userRepository.findByUsername(parseToken.getSubject());
         return new UserResponseDto(user);
+    }
+
+    @Transactional
+    public Map<String, Object> getPersonalInfo(Long id) {
+        Optional<UserEntity> user = userRepository.findById(id);
+        List<CategoryEntity> category = categoryRepository.findByUser(id);
+        List<BoardEntity> board = boardRepository.findByUser(user.get().getUsername());
+
+        List<CategoryDto> categoryDtoList = new ArrayList<>();
+        for(CategoryEntity categoryEntity : category){
+            CategoryDto cate = CategoryDto.builder()
+                    .name(categoryEntity.getName())
+                    .user(categoryEntity.getUser())
+                    .category(categoryEntity.getCategory())
+                    .build();
+            categoryDtoList.add(cate);
+        }
+
+        List<BoardResponseDto> boardResponseDtoList = new ArrayList<>();
+        boardService.getBoardList(board, boardResponseDtoList);
+
+        Map<String, Object> getPersonalInfoMap = new HashMap<>();
+        getPersonalInfoMap.put("profile", user.get().getProfile());
+        getPersonalInfoMap.put("nickname", user.get().getNickname());
+        getPersonalInfoMap.put("category", categoryDtoList);
+        getPersonalInfoMap.put("board", boardResponseDtoList);
+
+        return getPersonalInfoMap;
     }
 
 }
